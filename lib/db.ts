c92b8a6c174bf387a6dafa2bf85640d3d71d8be4
josue david kennedy <104701497+programfive@users.noreply.github.com>
2025-06-06 00:35:46 -0400

    error de configuracion prisma

--- lib/db.ts
+++ lib/db.ts
@@ -1,13 +1,10 @@
 import { PrismaClient } from './generated/prisma/client'
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
 }
 
-export const db =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ['query'],
-  })
+export const db = globalThis.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = db;
